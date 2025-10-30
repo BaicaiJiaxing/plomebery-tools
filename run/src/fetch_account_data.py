b10b0dd791eab7e245dd321ecd19e5e2b1cb3484
BAICAI <tongmengping@shwpg.com>
2025-10-30 18:21:10 +0800

    增加导出远传出账明细

--- run/src/fetch_account_data.py
+++ run/src/fetch_account_data.py
@@ -14,6 +14,33 @@ job_name = "fetch_account_data"
 job_config = config.get_config_by_job(job_name)
 logger = logging.getLogger(__name__)
 
+
+def fill_sql(job_config):
+    """填充config中的sql参数并返回最终可执行的sql"""
+    sql_template = job_config.get("sql")
+
+    # 获取当前日期
+    today = datetime.date.today()
+    current_month = today.strftime("%Y%m")  # 202509 -> mr_month 格式
+
+    # 上一年同月
+    last_year_date = today.replace(year=today.year - 1)
+    last_year_month = last_year_date.strftime("%Y-%m")
+
+    # 上一个月
+    first_day_of_this_month = today.replace(day=1)
+    previous_month_date = first_day_of_this_month - datetime.timedelta(days=1)
+    previous_month = previous_month_date.strftime("%Y-%m")
+
+    # 填充 SQL 模板
+    sql_filled = sql_template.format(
+        previous=previous_month_date.year,
+        previous_month=previous_month,
+        last_year=last_year_date.year,
+        last_year_month=last_year_month
+    )
+    return sql_filled
+
 def fetch_data_by_company(company: str):
     """按分公司获取数据"""
     company_config =  config.get_database(company)
@@ -40,6 +67,10 @@ def fetch_data_by_company(company: str):
 
     logger.info("处理后数据：%s", res)
     return res
+def build_excel():
+    '''查询出来的结果写入到excel'''
+
+
 
 def build_sms_message():
     # 从config.yaml中获取该定时任务需要执行的分公司名称，注意获取数据并组装成短信内容
@@ -59,12 +90,11 @@ def build_sms_message():
     return message
 
 # 注册到plombery
-@task
+# @task
 def fetch_account_data_job():
-    """定时任务：发送生产环境查表计划生成情况"""
+    """定时任务：发送生产环境远传出账生成情况"""
     logger = get_logger()
     message = build_sms_message()
-    logger.info(message)
     sms_client.send_sms(phones=job_config['phones'],content=message,logger=logger)
 
 
